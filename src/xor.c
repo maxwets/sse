fbe75dbd891f853badd7b29046c604e490e35236
Max Wets <max.wets@gmail.com>
2021-04-22 16:41:11 +0200

    t'es un porc francis

--- src/xor.c
+++ src/xor.c
@@ -6,13 +6,13 @@
 void printxor(){
 	char* str = "\x46\x68\x77\x64\x21\x6c\x64\x21\x63\x68\x75\x62\x6e\x68\x6f\x72\x20";
 	char xor = (char) 1;
-	for(int i=0; i<strlen(str); i++){
+	for(size_t i=0; i<strlen(str); i++){
 		printf("%c", str[i]^xor);
 	}
 	printf("\n");
 }
 
-int main(int argc, char** argv){
+int main(){
 	printxor();
 	return 0;
 }
